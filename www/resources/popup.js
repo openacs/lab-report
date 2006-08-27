2df9379bd30446cfaddc6eb7ce707d17b0004f8c
Nick Carroll <nick.c@rroll.net>
2006-08-27 03:42:54 +0000

    Added extra options to popup script.

--- www/resources/popup.js
+++ www/resources/popup.js
@@ -1,11 +1,11 @@
 function popup(url, title)
 {
-	var width = 250;
+	var width = 380;
 	var height = 300;
 	var x_pos = 0;
 	var y_pos = 0;
 	x_pos = 300;
 	y_pos = 200;
-	var popup_param = "dependent=yes,width=" + width + ",height=" + height + ",screenX="+x_pos +",screenY=" + y_pos+",left="+x_pos+",top="+y_pos+",titlebar=yes";
+	var popup_param = "scrollbars=yes,resizable=no,status=no,menubar=no,toolbar=no,dependent=yes,width=" + width + ",height=" + height + ",screenX="+x_pos +",screenY=" + y_pos+",left="+x_pos+",top="+y_pos+",titlebar=yes";
 	window.open(url, title, popup_param);
 }
